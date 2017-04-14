2a09de3531b45f4efef4cf311350a2db0a50809a
Arviens <gezzerr@gmail.com>
2017-04-15 00:50:34 +0300

    Tests

--- CodeChecker.Tests/Properties/AssemblyInfo.cs
+++ CodeChecker.Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyTitle("CodeChecker.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyProduct("CodeChecker.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("d424e5e5-d79b-4ef0-94c5-f73531800f96")]
+[assembly: Guid("86b61717-e5a3-47a1-ada9-2d8ac1d47bb1")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-export FrameworkPathOverride=/usr/lib/mono/4.5/
 dotnet restore && dotnet restore Tests
-cd Tests
-dotnet xunit
+dotnet test
