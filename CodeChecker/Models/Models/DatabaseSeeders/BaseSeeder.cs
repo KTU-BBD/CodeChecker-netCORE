d3416c8db8cfcad737a313dfb4fba44565101efc
Arviens <gezzerr@gmail.com>
2017-04-15 22:08:11 +0300

    Removed unused stuff

--- CodeChecker/Models/Models/DatabaseSeeders/BaseSeeder.cs
+++ CodeChecker/Models/Models/DatabaseSeeders/BaseSeeder.cs
@@ -7,19 +7,15 @@ namespace CodeChecker.Models.Models.DatabaseSeeders
 {
     public class BaseSeeder
     {
-        private UserManager<ApplicationUser> _userManager;
-        private RoleManager<IdentityRole> _roleManager;
-        protected ApplicationDbContext _context { get; set; }
-        private UserSeeder _userSeeder;
-        private RoleSeeder _roleSeeder;
+        private readonly UserSeeder _userSeeder;
+        private readonly RoleSeeder _roleSeeder;
+
         public BaseSeeder(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
         {
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _context = context;
             _userSeeder = new UserSeeder(userManager, roleManager, context);
-            _roleSeeder = new RoleSeeder(_roleManager);
+            _roleSeeder = new RoleSeeder(roleManager);
         }
+
         // Do not change the order of called methods
         public async Task EnsureSeedData()
         {
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-dotnet restore && dotnet restore Tests
-cd CodeChecker.Tests
-dotnet test
