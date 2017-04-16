2adfe2aef16c7a78809f0a6d7a6d71d3a2dc16e3
Arviens <gezzerr@gmail.com>
2017-04-16 13:42:54 +0300

    DEV-7 Added repository

---
+++ CodeChecker/Models/Repositories/ApplicationUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using CodeChecker.Data;
+
+namespace CodeChecker.Models.Repositories
+{
+    public class ApplicationUserRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ApplicationUserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ApplicationUser GetUser(string userName)
+        {
+            return _context.Users.First(x => x.UserName == userName);
+        }
+    }
+}
