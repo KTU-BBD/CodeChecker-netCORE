5a823527aa913193a35570c5d15ebc269a9a5cd5
Arviens <gezzerr@gmail.com>
2017-05-08 12:48:34 +0300

    Migration files

---
+++ CodeChecker/Data/Migrations/20170508065339_ContestType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeChecker.Migrations
+{
+    public partial class ContestType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Contests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Contests");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Assignments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
